22d01ffa8804301d9b8cd23f9cd959af4b1ef584
CasperPlate <casper.plate@hotmail.com>
2017-09-11 12:53:41 +0200

    les3

--- les03/eo 5_3.py
+++ les03/eo 5_3.py
@@ -1,1 +1,19 @@
-input('Geef een woord: ');
+woord = input('Geef een woord: ');
+E = 0;
+O = 0;
+I = 0;
+
+for i in woord:
+    if i == 'e':
+        E+=1;
+    if i == 'o':
+        O+=1;
+    if i == 'i':
+        I+=1;
+
+if E != 0:
+    print('Het woord bevat ' + str(E) + ' keer de letter e.')
+if O != 0:
+    print('Het woord bevat ' + str(O) + ' keer de letter 0.')
+if I != 0:
+    print('Het woord bevat ' + str(I) + ' keer de letter i.')
