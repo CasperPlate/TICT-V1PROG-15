5cb62283d5cfdc338d786f8423ea8fee759538e5
CasperPlate <casper.plate@hotmail.com>
2017-09-21 17:24:22 +0200

    huiswerk les 5 voortgang

---
+++ les05/pe5_4.py
@@ -0,0 +1,13 @@
+import time
+
+try:
+    hardlopers = open('hardlopers.txt', 'a')
+
+except FileNotFoundError:
+    hardlopers = open('hardlopers.txt', 'a')
+
+i = 0
+while i < 5:
+    hardlopers.write(time.strftime('%a %d %b %Y, %H:%M:%S, ', time.localtime()) + input('Geef naam: ') + '\n')
+    i = i + 1
+hardlopers.close()
